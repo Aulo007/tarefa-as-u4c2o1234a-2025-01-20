d229bc5b8a2828f1a36803e4051c6ab2b6d8a783
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-24 20:48:08 -0300

    Adiciona buffer de pixels global e função para inicializar o PIO

--- matriz_rgb.c
+++ matriz_rgb.c
@@ -6,3 +6,29 @@
 
 #define LED_COUNT 25
 
+// Buffer de pixels global
+static npLED_t leds[LED_COUNT];
+static PIO np_pio;
+static uint sm;
+
+void npInit(uint8_t pin)
+{
+    // Cria programa PIO.
+    uint offset = pio_add_program(pio0, &ws2818b_program);
+    np_pio = pio0;
+
+    // Toma posse de uma máquina PIO.
+    sm = pio_claim_unused_sm(np_pio, false);
+    if (sm < 0)
+    {
+        np_pio = pio1;
+        sm = pio_claim_unused_sm(np_pio, true);
+    }
+
+    // Inicia programa na máquina PIO obtida.
+    ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+    // Limpa buffer de pixels.
+    npClear();
+}
+
