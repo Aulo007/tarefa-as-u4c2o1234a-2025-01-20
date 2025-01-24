90a4690aba17e65ba9416c3098189582f72f76cf
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-24 20:28:40 -0300

    Configura pinos do Keypad

---
+++ keypad.h
@@ -0,0 +1,16 @@
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include "pico/stdlib.h"
+
+// Configuração das portas GPIO para linhas e colunas
+#define COL1 9
+#define COL2 8
+#define COL3 7
+#define COL4 6
+#define ROW1 13
+#define ROW2 12
+#define ROW3 11
+#define ROW4 10
+
+#endif // KEYPAD_H
