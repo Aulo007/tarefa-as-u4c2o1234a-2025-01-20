d2f3188e21d144da5f786ed9f9c1e61e4163ce18
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 20:19:06 -0300

    Inclusão da animação C

---
+++ animationC.c
@@ -0,0 +1,36 @@
+#include "matriz_rgb.h"
+#include "animationC.h"
+#include "pico/stdlib.h"
+#include "keypad.h"
+
+uint8_t matrizC[5][5][3] = 
+{
+{{204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}}, 
+{{204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}},
+{{204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}},
+{{204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}},
+{{204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}, {204, 0, 0}}
+};
+
+void animar_desenhos_C(void){
+
+  int i=0,j=0,k=0;
+  char tecla;
+
+  while(true){
+    tecla = keypad_read();
+    if((tecla != 'C')&&(tecla !='X')){
+        npClear2();
+        break;
+    }
+    for(int i = 0;i<15;i++){
+      tecla = keypad_read();
+        if((tecla != 'C')&&(tecla !='X')){
+          npClear2();
+          break;
+        }
+        npWrite2(matrizC);
+        sleep_ms(200);
+    }
+  }
+}
---
+++ animationC.h
@@ -0,0 +1,8 @@
+#ifndef ANIMATION2_H
+#define ANIMATION2_H
+
+extern uint8_t matrizC[5][5][3];
+
+void animar_desenhos_C();
+
+#endif
