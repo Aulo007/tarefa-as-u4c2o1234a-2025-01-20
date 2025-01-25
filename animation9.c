39db3a57b16b6bc901b0db1d1f05b77160e3b884
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-24 21:23:20 -0300

    Implementa função animar_desenhos_9 para animar desenhos a uma taxa de FPS até outro botão ser pressionado
    - Criação da função animar_desenhos_9 para animar o desenho gerado pela função caixa_de_desenhos.
    - Configuração da taxa de frames por segundo (FPS) para a animação.
    - Implementação de lógica para continuar a animação até que outro botão seja pressionado.

--- animation9.c
+++ animation9.c
@@ -24,3 +24,32 @@ int caixa_de_desenhos[2][ROWS][COLS][COLORS] = {
         {{255, 0, 0}, {255, 0, 0}, {255, 0, 0}, {255, 0, 0}, {255, 0, 0}},
         {{255, 0, 0}, {255, 0, 0}, {255, 255, 255}, {255, 0, 0}, {255, 0, 0}},
         {{255, 0, 0}, {255, 255, 255}, {255, 255, 255}, {255, 255, 255}, {255, 0, 0}}}};
+
+void animar_desenhos_9(void)
+{
+    while (true)
+    {
+        // Verifica a tecla antes de cada quadro de animação
+        char key_atual = keypad_read();
+        if (key_atual != '1' && key_atual != 'X')
+        {
+            npClear(); // Limpa os LEDs
+            return;    // Sai da animação se a tecla for diferente de '1'
+        }
+
+        for (int i = 0; i < DESENHOS; i++)
+        {
+            // Verifica a tecla a cada quadro, antes de mostrar o próximo desenho
+            key_atual = keypad_read();
+            if (key_atual != '1' && key_atual != 'X')
+            {
+                npClear(); // Limpa os LEDs
+                return;    // Sai da animação se a tecla for diferente de '1'
+            }
+
+            setMatrizDeLEDSComIntensidade(caixa_de_desenhos[i], 1, 1, 1.0);
+            npWrite();         // Atualiza a matriz de LEDs
+            sleep_ms(PERIODO); // Controla o tempo entre cada quadro
+        }
+    }
+}
