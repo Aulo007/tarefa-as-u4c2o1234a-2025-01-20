ea2d8ea4247713a110886494206b57c0cf5677d5
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 20:15:54 -0300

    Inclusão da animação C

--- animation2.h
+++ animation2.h
@@ -1,13 +1,8 @@
 #ifndef ANIMATION2_H
 #define ANIMATION2_H
 
-#define ROWS 5
-#define COLS 5
-#define COLORS 3
-
-
 extern int caixa_de_entrada[15][25];
-extern uint8_t matriz[15][5][5][3];
+extern uint8_t matriz2[15][5][5][3];
 
 
 void animar_desenhos_2();
