e1974e07fa9cbaa16a11b5c63c08210c6d918d2a
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 23:21:21 -0300

    Comentários e update

--- animationD.c
+++ animationD.c
@@ -3,6 +3,7 @@
 #include "pico/stdlib.h"
 #include "keypad.h"
 
+//Matriz de elementos dispostos similarmente aos LED's e que os configuram
 uint8_t matrizD[5][5][3] = 
 {
 {{0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}}, 
@@ -14,20 +15,13 @@ uint8_t matrizD[5][5][3] =
 
 void animar_desenhos_D(void){
 
-  int i=0,j=0,k=0;
   char tecla;
 
+  npWrite2(matrizD); //"Imprime" nos LED's como na 'matrizD'
   while(true){
-    tecla = keypad_read();
+    tecla = keypad_read(); //Verifica qual tecla é precionada constantemente para poder interromper a função
     if((tecla != 'D')&&(tecla !='X')){
         break;
-    }
-    for(int i = 0;i<15;i++){
-      tecla = keypad_read();
-        if((tecla != 'D')&&(tecla !='X')){
-          break;
-        }
-        npWrite2(matrizD);
     }
   }
 }
