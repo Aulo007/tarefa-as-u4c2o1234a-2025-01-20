5b3d70b909cd4c122fafec77e133d7a9cb948ea5
Igor Almeida <igoralmeida0105@gmail.com>
2025-01-26 15:34:15 -0300

    Termina arquivo 'animation7.h'

--- animation7.h
+++ animation7.h
@@ -1,3 +1,13 @@
 #ifndef ANIMATION7_H
 #define ANIMATION7_h
 
+#define ROWS 5
+#define COLS 5
+#define COLORS 3
+
+// Definição da caixa de desenhos
+extern int caixa_de_desenhos[9][ROWS][COLS][COLORS];
+// Definição da função de animar desenhos
+void animar_desenhos_7(char key_de_ativacao);
+
+#endif
