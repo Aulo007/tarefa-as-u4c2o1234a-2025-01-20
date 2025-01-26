746bff040fe17ffe562ca82aeaed95ebaf6cbadc
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-25 22:46:14 -0300

    Criação de função para exibir menu dinamicamente e limpar o terminal
    - Implementada função responsável por exibir o menu de acordo com a quantidade de itens disponíveis.
    - A função também limpa o terminal de forma dinâmica antes de exibir o menu.

--- menu.c
+++ menu.c
@@ -8,3 +8,14 @@ bool menu_is_valid_option(const MenuOption* options, int num_options, char input
     }
     return false;
 }
+
+void menu_display(const MenuOption* options, int num_options) {
+    printf("\033[2J\033[H");
+    printf("=== Sistema Iniciado ===\n");
+    printf("Pressione uma tecla para escolher uma opcao:\n\n");
+
+    for (int i = 0; i < num_options; i++) {
+        printf("%c: %s\n", options[i].key, options[i].description);
+    }
+    printf("\n");
+}
