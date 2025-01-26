cdc00f75dde7d2413ba81f5dc0110094515977f7
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 23:07:03 -0300

    Comentários e update

--- animation2.h
+++ animation2.h
@@ -1,11 +1,11 @@
 #ifndef ANIMATION2_H
 #define ANIMATION2_H
 
-extern int caixa_de_entrada[15][25];
-extern uint8_t matriz2[15][5][5][3];
+//extern int caixa_de_entrada[15][25];   Define matriz que foi descartada(explicação no animation2.c)
+extern uint8_t matriz2[15][5][5][3]; //Define matriz de sprites
 
 
-void animar_desenhos_2();
-void convertToRGB(int entrada, uint8_t matriz[3]);
+//void convertToRGB(int entrada, uint8_t matriz[3]);   Define função que foi descartada(explicação no animation2.c)
+void animar_desenhos_2(); //Define função que realiza a animação relacionada ao botão 2
 
 #endif
