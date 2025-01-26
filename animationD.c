a990bb05c010622dc7b8e14953edcc19bb75fb11
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 22:21:31 -0300

    Inclusão da animação D

---
+++ animationD.c
@@ -0,0 +1,35 @@
+#include "matriz_rgb.h"
+#include "animationD.h"
+#include "pico/stdlib.h"
+#include "keypad.h"
+
+uint8_t matrizD[5][5][3] = 
+{
+{{0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}}, 
+{{0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}},
+{{0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}},
+{{0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}},
+{{0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}, {0, 127, 0}}
+};
+
+void animar_desenhos_D(void){
+
+  int i=0,j=0,k=0;
+  char tecla;
+
+  while(true){
+    tecla = keypad_read();
+    if((tecla != 'D')&&(tecla !='X')){
+        npClear2();
+        break;
+    }
+    for(int i = 0;i<15;i++){
+      tecla = keypad_read();
+        if((tecla != 'D')&&(tecla !='X')){
+          npClear2();
+          break;
+        }
+        npWrite2(matrizD);
+    }
+  }
+}
---
+++ animationD.h
@@ -0,0 +1,8 @@
+#ifndef ANIMATIOND_H
+#define ANIMATIOND_H
+
+extern uint8_t matrizD[5][5][3];
+
+void animar_desenhos_D();
+
+#endif
