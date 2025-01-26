e7b6b01678d6bb0ad04a1df02c11342f75045842
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 23:16:07 -0300

    Comentários e update

--- animationB.c
+++ animationB.c
@@ -3,6 +3,7 @@
 #include "pico/stdlib.h"
 #include "keypad.h"
 
+//Matriz de elementos dispostos similarmente aos LED's e que os configuram
 uint8_t matrizB[5][5][3] = 
 {
 {{0, 0, 255}, {0, 0, 255}, {0, 0, 255}, {0, 0, 255}, {0, 0, 255}}, 
@@ -17,17 +18,11 @@ void animar_desenhos_B(void){
   int i=0,j=0,k=0;
   char tecla;
 
+  npWrite2(matrizB); //"Imprime" nos LED's como na 'matrizB'
   while(true){
-    tecla = keypad_read();
+    tecla = keypad_read(); //Verifica qual tecla é precionada constantemente para poder interromper a função
     if((tecla != 'B')&&(tecla !='X')){
-        break;
-    }
-    for(int i = 0;i<15;i++){
-      tecla = keypad_read();
-        if((tecla != 'B')&&(tecla !='X')){
-          break;
-        }
-        npWrite2(matrizB);
+      break;
     }
   }
 }
