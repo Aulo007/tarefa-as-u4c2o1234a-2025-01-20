fc837818559beb6780deb490f9e2da0469590f8e
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-25 23:22:53 -0300

    Comentários

--- animationD.h
+++ animationD.h
@@ -1,8 +1,8 @@
 #ifndef ANIMATIOND_H
 #define ANIMATIOND_H
 
-extern uint8_t matrizD[5][5][3];
+extern uint8_t matrizD[5][5][3]; //Define matriz de "intruções" aos LED's
 
-void animar_desenhos_D();
+void animar_desenhos_D(); //Define função relacionada ao botão D
 
 #endif
