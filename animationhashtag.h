f27e0e2c5c9e8d45e11b6dabae8a353fc4072d20
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-26 21:55:44 -0300

    Muda intensidade, coloca cor branca com leds 20%

--- animationhashtag.h
+++ animationhashtag.h
@@ -6,7 +6,7 @@
 #define COLORS 3
 
 // Definição da caixa de desenhos
-extern int caixa_de_desenhos_hashtag[4][ROWS][COLS][COLORS];
+extern int caixa_de_desenhos_hashtag[1][ROWS][COLS][COLORS];
 // Definição da função de animar desenhos
 void animar_desenhos_hashtag(char key_de_ativacao);
 
